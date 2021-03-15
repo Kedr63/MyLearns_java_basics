8c5da11b2b667ea91667262afd48d33b7f968168
Aleksandr Shabalin <satria@mail.ru>
2021-03-15 20:45:40 +0300

    homework 4_3: Change TrucksContainersAndBoxes _use one for()

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+
     final static int CONTAINER_IN_TRUCK = 12;
     final static int BOXES_IN_CONTAINER = 27;
 
@@ -9,38 +10,25 @@ public class Main {
         String boxes = scanner.nextLine();
         int countBox = Integer.parseInt(boxes);  //обработаем, введеную с внешнего мира, строку в число int
 
-        // первый вариант вычисления количества контейнеров и грузовиков
-        int countContainer = (int) Math.ceil((double) countBox / BOXES_IN_CONTAINER);
-        int countTruck = (int) Math.ceil((double) countContainer / CONTAINER_IN_TRUCK);
-
-        // второй вариант вычисления количества контейнеров и грузовиков
-       /* int countContainer = (countBox / Container.BOXES_IN_CONTAINER)
-                + (int) Math.signum((double) (countBox % Container.BOXES_IN_CONTAINER));
-        int countTruck = (countContainer / Truck.CONTAINER_IN_TRUCK)
-                + (int) Math.signum((double) (countContainer % Truck.CONTAINER_IN_TRUCK));*/
-
-
-        int x = 1;
-        int y = 1;
-        for (int i = 1; i <= countTruck; i++) {
-            System.out.println("Грузовик: " + i);
-
-            for (int i1 = 1; i1 <= CONTAINER_IN_TRUCK; i1++) {
-                if (x <= countContainer) {
-                    System.out.println("\tКонтейнер: " + x);
-                    x++;
-                }
-                for (int i2 = 1; i2 <= BOXES_IN_CONTAINER; i2++) {
-                    if (y <= countBox) {
-                        System.out.println("\t\tЯщик: " + y);
-                        y++;
-                    }
-                }
+        int countTruck = 0;
+        int countContainer = 0;
+        for (int i = 1; i <= countBox; i++) {
+            if (i % (BOXES_IN_CONTAINER * CONTAINER_IN_TRUCK) == 1) {
+                countTruck++;
+                System.out.println("Грузовик: " + countTruck);
             }
+            if (i % BOXES_IN_CONTAINER == 1) {
+                countContainer++;
+                System.out.println("\tКонтейнер: " + countContainer);
+            }
+            System.out.println("\t\tЯщик: " + i);
         }
+
         System.out.println("Необходимо:"
                 + System.lineSeparator() + "грузовиков - " + countTruck + " шт."
                 + System.lineSeparator() + "контейнеров - " + countContainer + " шт.");
+
+
 
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
