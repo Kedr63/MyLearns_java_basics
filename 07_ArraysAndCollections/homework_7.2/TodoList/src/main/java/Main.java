e0c87cd42bc0b552f5c645d0fc2371280593c542
Aleksandr Shabalin <satria@mail.ru>
2021-05-20 08:11:26 +0300

    homework_7.2: todoList

--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
     private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String todo = scanner.nextLine();
+            if (todo.contains("exit")) {
+                break;
+            }
+
+            if (todo.contains("ADD")) {
+                if (todo.matches("ADD(\\s+)?\\d+.+")) {
+                    int index = Integer.parseInt(todo.replaceAll("ADD(\\s+)?(\\d+).+", "$2"));
+                    todoList.add(index, todo);
+                } else todoList.add(todo);
+            }
+
+            if (todo.contains("LIST")) {
+                for (int i = 0; i < todoList.getTodos().size(); i++) {
+                    System.out.println(i + " - " + todoList.getTodos().get(i));
+                }
+            }
+
+            if (todo.contains("EDIT")) {
+                int index = Integer.parseInt(todo.replaceAll("EDIT(\\s+)?(\\d+).+", "$2"));
+                todoList.edit(todo, index);
+            }
+
+            if (todo.contains("DELETE")) {
+                int index = Integer.parseInt(todo.replaceAll("DELETE(\\s+)?(\\d+)", "$2"));
+                todoList.delete(index);
+            }
+        }
     }
 }
--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
@@ -1,29 +1,40 @@
 import java.util.ArrayList;
 
 public class TodoList {
+    ArrayList<String> stringArrayList = new ArrayList<>();
 
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        stringArrayList.add(todo.replaceAll("ADD", "").trim());
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+        if (index < stringArrayList.size()){
+            stringArrayList.add(index, todo.replaceAll("ADD(\\s+)?\\d+", "").trim());
+        } else stringArrayList.add(todo.replaceAll("ADD(\\s+)?\\d+", "").trim());
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (index < stringArrayList.size()) {
+            stringArrayList.remove(index);
+            stringArrayList.add(index, todo.replaceAll("EDIT(\\s+)?\\d+", "").trim());
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (index < stringArrayList.size()) {
+            stringArrayList.remove(index);
+        }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        return stringArrayList;
     }
-
 }
