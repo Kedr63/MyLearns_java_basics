3479eee13d46d4a49c0398fea534a91aab21aced
Aleksandr Shabalin <satria@mail.ru>
2021-05-23 12:48:23 +0300

    homework_7.2: correct todoList

--- 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
+++ 07_ArraysAndCollections/homework_7.2/TodoList/src/main/java/TodoList.java
@@ -1,40 +1,45 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class TodoList {
-    ArrayList<String> stringArrayList = new ArrayList<>();
+    private final List<String> stringList = new ArrayList<>();
 
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
-        stringArrayList.add(todo.replaceAll("ADD", "").trim());
+        stringList.add(todo.replaceAll("ADD", "").trim());
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
-        if (index < stringArrayList.size()){
-            stringArrayList.add(index, todo.replaceAll("ADD(\\s+)?\\d+", "").trim());
-        } else stringArrayList.add(todo.replaceAll("ADD(\\s+)?\\d+", "").trim());
+        if (validateIndex(index)) {
+            stringList.add(index, todo.replaceAll("ADD(\\s+)?\\d+", "").trim());
+        } else stringList.add(todo.replaceAll("ADD(\\s+)?\\d+", "").trim());
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
-        if (index < stringArrayList.size()) {
-            stringArrayList.remove(index);
-            stringArrayList.add(index, todo.replaceAll("EDIT(\\s+)?\\d+", "").trim());
+        if (validateIndex(index)) {
+            stringList.remove(index);
+            stringList.add(index, todo.replaceAll("EDIT(\\s+)?\\d+", "").trim());
         }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
-        if (index < stringArrayList.size()) {
-            stringArrayList.remove(index);
+        if (validateIndex(index)) {
+            stringList.remove(index);
         }
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return stringArrayList;
+        return (ArrayList<String>) stringList;
+    }
+
+    public boolean validateIndex (int index){
+        return index < stringList.size();
     }
 }
